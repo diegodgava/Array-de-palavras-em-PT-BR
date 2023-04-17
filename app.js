23e7f01621e73f882b4b825ef052cdefa8f79d15
Diego Gava <diego.gava15@gmail.com>
2023-04-17 01:53:59 -0300

    subindo arquivos

---
+++ app.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+
+// Lê o arquivo de texto com a lista de palavras
+fs.readFile('palavras.txt', 'utf-8', (err, data) => {
+  if (err) throw err;
+
+  // Separa as palavras por quebra de linha e cria um array
+  const arrayDePalavras = data.split('\n');
+
+  // Filtra o array para criar um novo array com palavras que atendem aos critérios
+  const palavrasFiltradas = arrayDePalavras.filter(palavra => {
+// insira todos os filtros que desejar
+    
+  });
+
+  // Cria um novo arquivo e escreve o novo array nele
+  fs.writeFile('todas_as_palavras.txt', `module.exports = ${JSON.stringify(palavrasFiltradas)};`, (err) => {
+    if (err) throw err;
+    console.log('O arquivo foi salvo com sucesso!');
+  });
+});
+
